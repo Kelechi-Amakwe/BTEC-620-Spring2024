c05e543fcbee2443bbda1652f542a0741a27e314
Kelechi Amakwe <122706853+Kelechi-Amakwe@users.noreply.github.com>
2024-01-25 14:06:49 -0500

    Add files via upload

---
+++ January--23.R
@@ -0,0 +1,37 @@
+#Author:Kelechi Amakwe, Date: 01/23/2024, Purpose: Perform microarray analysis
+
+#Set the working directory
+setwd("/Users/kelly/Documents/Data")
+
+#Load library Affy
+library(affy)
+
+#Read the .CEL data files
+data <- ReadAffy()
+
+#Visualize data
+boxplot(data)
+
+#Perform RMA normalization
+data_norm <-rma(data)
+
+#Extract the expression data
+data_norm_exprs <-exprs(data_norm)
+
+#Plot the normalized dataset
+boxplot(data_norm_exprs)
+
+#Take averages for treatment and controls for fold change analysis
+head(data_norm_exprs)
+Control <- data_norm_exprs[,c(1:2)]
+Treatment <- data_norm_exprs[,c(3:4)]
+head(Control)
+head(treatment)
+
+Control <-rowMeans(Control)
+Treatment <-rowMeans(Treatment)
+
+#Take the fold change
+Fold_change <-Treatment-Control
+
+
